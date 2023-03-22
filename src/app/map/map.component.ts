a30865023673bf1bed7262254e1b0ee4144b3db1
sawyer kokesh <sawyerkokeshw@gmail.com>
2023-03-22 14:15:16 -0700

    added hover actions to legend, fix how svg is displayed on map page

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { defaultLocaleWeekdays } from 'ngx-bootstrap/chronos/locale/locale.class';
 
 @Component({
   selector: 'app-map',
@@ -14,42 +15,86 @@ export class MapComponent implements OnInit {
 
   }
 
-  loadComponent(){
+  loadComponent() {
     var svg = document.getElementById("imperiumMap");
     var grainList = svg.getElementsByClassName("grain");
     var ironList = svg.getElementsByClassName("iron");
     var stoneList = svg.getElementsByClassName("stone");
     var livestockList = svg.getElementsByClassName("livestock");
     var woodList = svg.getElementsByClassName("wood");
+    var goldList = svg.getElementsByClassName("gold");
+    var moutainsList = svg.getElementsByClassName("moutains");
 
-    this.loadHoverEvents(grainList);
-    this.loadHoverEvents(ironList);
-    this.loadHoverEvents(stoneList);
-    this.loadHoverEvents(livestockList);
-    this.loadHoverEvents(woodList);
+    // var grainIdList = [];
+    // Array.from(grainList).forEach(e => {grainIdList.push(e.id)});
+    // Array.from(ironList).forEach(e => {ironIdList.push(e.id)});
+    // Array.from(stoneList).forEach(e => {stoneIdList.push(e.id)});
+    // Array.from(livestockList).forEach(e => {livestockIdList.push(e.id)});
+    // Array.from(woodList).forEach(e => {woodIdList.push(e.id)});
+    // console.log(grainIdList);
 
+    // console.log(document.getElementById("legend-grain"));
+
+    // grainList[1].parentNode.insertBefore(document.getElementById("legend-grain"), grainList[1].nextSibling);
+
+    this.loadHoverEvents(svg, grainList, moutainsList);
+    this.loadHoverEvents(svg, ironList, moutainsList);
+    this.loadHoverEvents(svg, stoneList, moutainsList);
+    this.loadHoverEvents(svg, livestockList, moutainsList);
+    this.loadHoverEvents(svg, woodList, moutainsList);
+    this.loadHoverEvents(svg, goldList, moutainsList);
+    this.loadHoverEvents(svg, moutainsList, moutainsList);
+
+    const mouseoverEvent = new Event('mouseover');
+    const mouseoutEvent = new Event('mouseout');
+
+    this.loadHoverLegendEvents("legend-grain", grainList, mouseoverEvent, mouseoutEvent);
+    this.loadHoverLegendEvents("legend-iron", ironList, mouseoverEvent, mouseoutEvent);
+    this.loadHoverLegendEvents("legend-stone", stoneList, mouseoverEvent, mouseoutEvent);
+    this.loadHoverLegendEvents("legend-livestock", livestockList, mouseoverEvent, mouseoutEvent);
+    this.loadHoverLegendEvents("legend-wood", woodList, mouseoverEvent, mouseoutEvent);
+    this.loadHoverLegendEvents("legend-gold", goldList, mouseoverEvent, mouseoutEvent);
+    this.loadHoverLegendEvents("legend-moutains", moutainsList, mouseoverEvent, mouseoutEvent);
   }
 
-  private loadHoverEvents(list: HTMLCollectionOf<Element>) {
+  private loadHoverLegendEvents(legendType: string, legendTypeList: HTMLCollectionOf<Element>, mouseoverEvent: Event, mouseoutEvent: Event) {
+    document.getElementById(legendType).addEventListener("mouseover", function test() {
+      legendTypeList[0].dispatchEvent(mouseoverEvent);
+    });
+    document.getElementById(legendType).addEventListener("mouseout", function test() {
+      legendTypeList[0].dispatchEvent(mouseoutEvent);
+    });
+  }
+
+  private loadHoverEvents(svg: HTMLElement, list: HTMLCollectionOf<Element>, moutains: HTMLCollectionOf<Element>) {
     var fillColor = list[0].getAttribute("fill");
     Array.from(list).forEach(e => {
       e.addEventListener("mouseover", function fillItem() {
         Array.from(list).forEach(f => {
-          f.setAttribute("fill", "#9EC2F7");
-          f.setAttribute("stroke-width", "3");
+          svg.appendChild(f);
+          // f.setAttribute("fill", "#9EC2F7");
+          f.setAttribute("stroke-width", "8");
+          // f.setAttribute("stroke", "blue");
+          f.setAttribute("filter", "url(#shadow)");
           // f.setAttribute("fill-opacity", "50%");
         });
       });
       e.addEventListener("mouseout", function fillItem() {
         Array.from(list).forEach(f => {
-          f.setAttribute("fill", fillColor);
+          Array.from(moutains).forEach(m => {
+            svg.appendChild(m);
+          });
+
+          // f.setAttribute("fill", fillColor);
           f.setAttribute("stroke-width", "1");
+          // f.setAttribute("stroke", "black");
+          f.setAttribute("filter", "");
           // f.setAttribute("fill-opacity", "100%");
         });
       });
     });
   }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
   }
 }
