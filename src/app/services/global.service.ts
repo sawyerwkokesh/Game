4a1a40c694245114e7503ccbfaaa5bb202b39314
sawyer kokesh <sawyerkokeshw@gmail.com>
2023-06-24 01:05:20 -0700

    Adding in the rest of the factions into the serviecs and constants

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -9,22 +9,47 @@ import { constCringeNames, constFactionNames } from 'src/constants/constants';
 
 export class GlobalService {
     public currentNames = new BehaviorSubject<any>({
-        knight: constFactionNames.knight
+        japan: constFactionNames.japan,
+        knight: constFactionNames.knight,
+        viking: constFactionNames.viking,
+        greek: constFactionNames.greek,
+        mongol: constFactionNames.mongol,
+        aztec: constFactionNames.aztec
     })
 
     public factionNames = new BehaviorSubject<any>({
-        knight: constFactionNames.knight
+        japan: constFactionNames.japan,
+        knight: constFactionNames.knight,
+        viking: constFactionNames.viking,
+        greek: constFactionNames.greek,
+        mongol: constFactionNames.mongol,
+        aztec: constFactionNames.aztec
     })
 
-    public cringeNames = new BehaviorSubject<any>({
-        knight: constCringeNames.knight
+    public cringeNames = new BehaviorSubject<any>({\
+        japan: constCringeNames.japan,
+        knight: constCringeNames.knight,
+        viking: constCringeNames.viking,
+        greek: constCringeNames.greek,
+        mongol: constCringeNames.mongol,
+        aztec: constCringeNames.aztec
     })
 
     updateToFactionNames(){
+        this.currentNames.value.japan = this.factionNames.value.japan;
         this.currentNames.value.knight = this.factionNames.value.knight;
+        this.currentNames.value.viking = this.factionNames.value.viking;
+        this.currentNames.value.greek = this.factionNames.value.greek;
+        this.currentNames.value.mongol = this.factionNames.value.mongol;
+        this.currentNames.value.aztec = this.factionNames.value.aztec;
     }
 
     updateToCringeNames(){
+        this.currentNames.value.japan = this.cringeNames.value.japan;
         this.currentNames.value.knight = this.cringeNames.value.knight;
+        this.currentNames.value.viking = this.cringeNames.value.viking;
+        this.currentNames.value.greek = this.cringeNames.value.greek;
+        this.currentNames.value.mongol = this.cringeNames.value.mongol;
+        this.currentNames.value.aztec = this.cringeNames.value.aztec;
     }
 }
